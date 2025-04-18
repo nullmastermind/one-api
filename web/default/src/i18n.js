2de23d840f0c201b47749c8b1477d8fc408d27d6
monkjiro <thenullmastermind@gmail.com>
2025-04-18 21:26:26 +0700

    refactor: update i18n configuration to change fallback language to English

--- web/default/src/i18n.js
+++ web/default/src/i18n.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import {initReactI18next} from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import zhTranslation from './locales/zh/translation.json';
 import enTranslation from './locales/en/translation.json';
@@ -8,21 +8,21 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'zh',
+    fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
 
     interpolation: {
       escapeValue: false,
     },
 
-      resources: {
-          zh: {
-              translation: zhTranslation
-          },
-          en: {
-              translation: enTranslation
-          }
-      }
+    resources: {
+      zh: {
+        translation: zhTranslation,
+      },
+      en: {
+        translation: enTranslation,
+      },
+    },
   });
 
 export default i18n;
